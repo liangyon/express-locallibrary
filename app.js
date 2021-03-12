929164f1ff1eafe4ca4ea86180b16f511694fe12
liangyon <yonlin.liang@mail.utoronto.ca>
2021-03-11 22:06:18 -0800

    added ui

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var coolRouter = require('./routes/cool');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var compression = require('compression');
@@ -19,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // mongoose setup
 var mongoose = require('mongoose');
-var dev_db_url ;
+var dev_db_url;
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cool', coolRouter);
 app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
